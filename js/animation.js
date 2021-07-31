284c969b6a1c9ed5c57b48b0eba6300d1ff10be5
stellashank <79393723+stellashank@users.noreply.github.com>
2021-07-31 23:08:41 +0530

    initaia

---
+++ js/animation.js
@@ -0,0 +1,37 @@
+
+
+let animated = document.querySelectorAll('.move-up-1, .move-up-2, .move-left');
+
+function showScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+
+    for (let i = 0; i < animated.length; i++) {
+        let scrollAnimated = animated[i].offsetTop;
+
+        if (animated[i].matches('.move-up-1')) {
+            if (scrollAnimated - 400 < scrollTop) {
+                animated[i].classList.add('js-remove-opacity');
+                animated[i].classList.add('js-move-up-1');
+            }
+        } else if (animated[i].matches('.move-up-2')) {
+            if (scrollAnimated + 100 < scrollTop) {
+                animated[i].classList.add('js-remove-opacity');
+                animated[i].classList.add('js-move-up-2');
+            }
+        } else if (animated[i].matches('.move-left')) {
+            if (scrollAnimated + 100 < scrollTop) {
+                animated[i].classList.add('js-remove-opacity');
+                animated[i].classList.add('js-move-left');
+            }
+        }
+    }
+}
+
+window.addEventListener('scroll', showScroll);
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
